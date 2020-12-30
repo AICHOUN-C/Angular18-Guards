429fdcfadc23af819304610074d1ea26b6b85e56
AICHOUN-C <cherif.aichoun@gmail.com>
2020-12-30 16:27:37 +0100

    feat: challenge done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashBoardComponent,
+    AdminPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
